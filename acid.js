943e9473bb7311b75db99844d6444951eb3ba52b
Matt Butcher <matt.butcher@microsoft.com>
2017-06-21 17:59:42 -0600

    Removed commented code.

--- acid.js
+++ acid.js
@@ -1,8 +1,7 @@
-// Acid CI/CD
 events.push = function(e) {
   console.log("===> Building " + e.repo.cloneURL + " " + e.commit);
 
-  node = new Job("node-runner")
+  var node = new Job("node-runner")
   node.image = "node:8"
   node.tasks = [
     "cd /src/hello",
@@ -10,14 +9,4 @@ events.push = function(e) {
     "node index.js"
   ]
   node.run()
-
-  /*
-  j = new Job("empty-testbed-test");
-  j.image = "acidic.azurecr.io/acid-ubuntu:latest";
-  j.tasks = [
-    "ls -lah",
-    "sleep 200"
-  ];
-  j.run()
-  */
 }
