5c4bc10b213058e97b23595a5af9316b645f685e
Matthew Fisher <matt.fisher@microsoft.com>
2017-09-01 14:33:02 -0700

    Update acid.js

--- acid.js
+++ acid.js
@@ -1,4 +1,4 @@
-//const {events, Job} = require("./libacid")
+const {events, Job} = require("libacid")
 
 events.on("push", function(e, project) {
   console.log("===> Building " + project.repo.cloneURL + " " + e.commit)
